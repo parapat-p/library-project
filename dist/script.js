da7e61a9750f0e0081cd5a37d6c18569545aa19a
parapat-p <parapat.patc@gmail.com>
2025-09-27 21:34:47 +0700

    Add createCard function

--- dist/script.js
+++ dist/script.js
@@ -1,18 +1,23 @@
 "use strict";
+// Type declaration END
 // const declaration START
 var myLibrary = [];
 var dialog = document.getElementById("dialog") || null;
 var addButton = document.getElementById("show-dialog");
-// Type declaration END
+var closeButton = dialog === null || dialog === void 0 ? void 0 : dialog.querySelector("#novalidate-close");
+var container = document.querySelector(".container");
+// const declaration END
 // Function declaration START
-function Book(bookName, author) {
-    var _newTarget = this && this instanceof Book ? this.constructor : void 0;
+function BookInstanceConstructor(bookName, author, page, isRead, id) {
+    var _newTarget = this && this instanceof BookInstanceConstructor ? this.constructor : void 0;
     if (!_newTarget) {
         throw new TypeError("Please use constructor new to create an Object!");
     }
-    this.uid = crypto.randomUUID();
+    this.uid = id;
     this.bookTitle = bookName;
     this.bookAuthor = author;
+    this.bookPage = page;
+    this.isRead = isRead;
 }
 function addBookToLibrary() {
 }
@@ -24,9 +29,50 @@ function activateDialogEvent() {
     addButton.addEventListener("click", function () {
         dialog.showModal();
     });
-    // closeButton.addEventListener("click", () => {
-    //     dialog.close();
-    // });
+    closeButton === null || closeButton === void 0 ? void 0 : closeButton.addEventListener("click", submitCloseButton);
+    function submitCloseButton(event) {
+        event.preventDefault();
+    }
+}
+function createCard(bookObject) {
+    var card = document.createElement("div");
+    card.className = "card";
+    var ATTR_ORDER = [
+        "bookTitle",
+        "bookAuthor",
+        "bookPage",
+        "isRead",
+        "uid"
+    ];
+    ATTR_ORDER.forEach(function (key) {
+        var p = document.createElement("p");
+        var topic = "";
+        switch (key) {
+            case "bookTitle":
+                topic = "Book Title";
+                break;
+            case "bookAuthor":
+                topic = "Book Author";
+                break;
+            case "bookPage":
+                topic = "Total pages";
+                break;
+            case "isRead":
+                topic = "Already read";
+                break;
+            case "uid":
+                topic = "Book ID";
+                break;
+        }
+        p.textContent = "".concat(topic, ": ").concat(bookObject[key]);
+        card.appendChild(p);
+    });
+    if (container) {
+        console.log("Add card!");
+        container.appendChild(card);
+    }
 }
 // Function declaration END
+// Initialize function event Start
 activateDialogEvent();
+// Initialize function event END
--- script.ts
+++ script.ts
@@ -1,31 +1,34 @@
-// const declaration START
-const myLibrary:BookInstance[] = [];
-const dialog = document.getElementById("dialog") as HTMLDialogElement || null;
-const addButton = document.getElementById("show-dialog");
-const closeButton = dialog?.querySelector("#novalidate-close");
-
-// const declaration END
-
 // Type declaration START
 type BookInstance = {
     uid: string;
     bookTitle: string;
     bookAuthor: string;
-    bookPage:number
+    bookPage:number;
+    isRead:boolean;
 };
-
 // Type declaration END
+
+
+// const declaration START
+const myLibrary:BookInstance[] = [];
+const dialog = document.getElementById("dialog") as HTMLDialogElement || null;
+const addButton = document.getElementById("show-dialog");
+const closeButton = dialog?.querySelector("#novalidate-close");
+const container = document.querySelector(".container");
+// const declaration END
 
 // Function declaration START
 
-function Book(this: BookInstance,bookName:string,author:string,page:number){
+function BookInstanceConstructor(this: BookInstance,bookName:string,author:string,page:number,isRead:boolean ,id:string){
     if(!new.target){
         throw new TypeError("Please use constructor new to create an Object!");
     }
-    this.uid = crypto.randomUUID();
+    this.uid = id;
     this.bookTitle = bookName;
     this.bookAuthor = author;
     this.bookPage = page;
+    this.isRead = isRead;
+    
 }
 
 function addBookToLibrary(){
@@ -48,11 +51,52 @@ function activateDialogEvent() {
     }
 }
 
+function createCard(bookObject:BookInstance){
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const ATTR_ORDER: (keyof BookInstance)[] = [
+        "bookTitle",
+        "bookAuthor",
+        "bookPage",
+        "isRead",
+        "uid"
+    ];
+    ATTR_ORDER.forEach((key) => {
+        const p = document.createElement("p");
+        let topic:string = "";
+        switch(key){
+            case "bookTitle":
+                topic = "Book Title";
+                break;
+            case "bookAuthor":
+                topic = "Book Author";
+                break;
+            case "bookPage":
+                topic = "Total pages";
+                break;
+            case "isRead":
+                topic = "Already read";
+                break;
+            case "uid":
+                topic = "Book ID";
+                break;
+        }
+        p.textContent = `${topic}: ${bookObject[key]}`;
+        card.appendChild(p);
+    });
+
+    if(container){
+        console.log("Add card!")
+        container.appendChild(card);
+    }
+
+}
+
 
 // Function declaration END
 
 // Initialize function event Start
 
 activateDialogEvent();
-
 // Initialize function event END
