79f9b1d6c18441bf7e8740432f11b245b4f1994a
parapat-p <parapat.patc@gmail.com>
2025-09-27 20:41:12 +0700

    Restruct class in card html

--- script.ts
+++ script.ts
@@ -11,19 +11,21 @@ type BookInstance = {
     uid: string;
     bookTitle: string;
     bookAuthor: string;
+    bookPage:number
 };
 
 // Type declaration END
 
 // Function declaration START
 
-function Book(this: BookInstance,bookName:string,author:string){
+function Book(this: BookInstance,bookName:string,author:string,page:number){
     if(!new.target){
         throw new TypeError("Please use constructor new to create an Object!");
     }
     this.uid = crypto.randomUUID();
     this.bookTitle = bookName;
     this.bookAuthor = author;
+    this.bookPage = page;
 }
 
 function addBookToLibrary(){
@@ -48,7 +50,8 @@ function activateDialogEvent() {
 
 // Function declaration END
 
-
-
+// Initialize function event Start
 
 activateDialogEvent();
+
+// Initialize function event END
